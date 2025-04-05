5967d37e0e05ce4d8ded95ec904320d9259fe37f
Zanyar <zanyarsalami@gmail.com>
2025-04-05 23:42:24 +0200

    a push example

--- sql_load/3_modify_tables.sql
+++ sql_load/3_modify_tables.sql
@@ -1,12 +1,27 @@
+WITH remote_job_skills AS(
+    SELECT
+        skill_id,
+        COUNT(*) AS skill_count
+    FROM
+        skills_job_dim AS skills_to_job
+    INNER JOIN job_postings_fact AS job_posting
+        ON job_posting.job_id = skills_to_job.job_id
+    WHERE
+        job_work_from_home = TRUE
+    GROUP BY
+        skill_id
+)
+
 SELECT
-    COUNT(job_id),
-    CASE
-        WHEN job_location = 'Anywhere' THEN 'Remote'
-        WHEN job_location LIKE '%Germany%' THEN 'Local'
-        ELSE 'Onsite'
-    END AS location_cat
+    skills.skill_id,
+    skills.skills,
+    skill_count
 FROM
-    job_postings_fact
-GROUP BY
-    location_cat
+    remote_job_skills
+INNER JOIN skills_dim AS skills
+    ON skills.skill_id = remote_job_skills.skill_id
+ORDER BY
+    skill_count DESC
+
 ;
+
---
+++ sql_load/project_luke/first_query.sql
@@ -0,0 +1,1 @@
+-- later would be deleted
